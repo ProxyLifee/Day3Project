18a9ef88785a70a6c4c7ac16b87a9c384dc11fdc
EU3 Batch <av.serdaralp@gmail.com>
2020-10-14 03:07:50 +0100

    Added new file for awesome feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,8 @@
+public class AwesomeTest {
+
+
+    //red means it is untracked
+    //green means it is  under staging area
+    //grey in .idea folder means it is ignored
+    //when the color is normal, means it is commited no changes
+}
