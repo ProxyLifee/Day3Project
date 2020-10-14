18a032aeb8695070b37ae0e97125df7f41b980e5
EU3 Batch <av.serdaralp@gmail.com>
2020-10-14 03:21:01 +0100

    added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,4 +5,9 @@ public class AwesomeTest {
     //green means it is  under staging area
     //grey in .idea folder means it is ignored
     //when the color is normal, means it is commited no changes
+
+    //I just pulled the new change from GitHub that i did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now i made more awesome changes and i am about to push this back to GitHub
 }
